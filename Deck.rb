7d8a9502141c230bb8cff2acb23bae361475716e
mazdai <you@example.com>
2020-10-10 13:44:07 +0400

    создал клас колоды поправил ошибки

---
+++ Deck.rb
@@ -0,0 +1,20 @@
+class Desk
+  CARD_SUIT = %w[♡ ♧ ♢ ♤].freeze
+  CARD_VALUE = %w[1 2 3 4 5 6 7 8 9 10 J Q K A].freeze
+
+  attr_reader :cards
+
+  def initialize
+    @cards = generate_card
+  end
+
+  private
+
+  def generate_card
+    CARD_SUIT.each do |suit|
+      CARD_VALUE.each do |value|
+        cards << Card.new(suit, value)
+      end
+    end
+  end
+end
